c075f2ab9a6f0ea228790ff08b99f1598f9aec91
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-10 13:22:53 +0900

    js if文

--- javalesson2/index.js
+++ javalesson2/index.js
@@ -1,2 +1,12 @@
-console.log("hello");
-alert("hello");
+// console.log("hello");
+// alert("hello");
+
+const animals = "penguin";
+
+if(animals == "penguin"){
+  alert("penguin");
+}else if(animals == "dog"){
+   alert("犬");
+}else{
+  alert("その他");
+}
